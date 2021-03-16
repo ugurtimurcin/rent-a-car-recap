912cc40cb04b09b60a2f52d81480178a8716b777
Ugur Timurcin <utimurcin@outlook.com>
2021-03-17 00:52:50 +0300

    Messages created.

---
+++ Business/Constants/Message.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Message
+    {
+        public static string AuthorizationDenied = "You do not have permisson to access";
+        public static string TokenCreated = "Token created";
+        public static string UserNotExists = "User not exists";
+        public static string PasswordError = "Password is not correct";
+        public static string UserRegistered = "User succesfully registered";
+        public static string UserExists = "User already exists";
+    }
+}
