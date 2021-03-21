031c1a5fc8ce2a0e5984a181d91225b1dab4591e
Ugur Timurcin <utimurcin@outlook.com>
2021-03-21 11:13:34 +0300

    File Helper (imagehelper) created.

---
+++ Core/Utilities/Helpers/FileHelpers/IFileHelper.cs
@@ -0,0 +1,16 @@
+﻿using Core.Utilities.Results;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Helpers.FileHelpers
+{
+    public interface IFileHelper
+    {
+        IResult Upload(string fileName, IFormFile file);
+        IResult Delete(string path);
+    }
+}
---
+++ Core/Utilities/Helpers/FileHelpers/ImageHelper.cs
@@ -0,0 +1,28 @@
+﻿using Core.Utilities.Results;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Helpers.FileHelpers
+{
+    public class ImageHelper : IFileHelper
+    {
+        public  IResult Delete(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Upload(string fileName, IFormFile file)
+        {
+            var imageName = fileName + Path.GetExtension(file.FileName);
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/CarImages/" + imageName);
+            var stream = new FileStream(path, FileMode.Create);
+            file.CopyTo(stream);
+            return new SuccessResult();
+        }
+    }
+}
