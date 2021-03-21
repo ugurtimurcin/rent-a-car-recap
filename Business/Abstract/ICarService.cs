c14c513f09966bf9040348e8c4605c6b167be1dd
Ugur Timurcin <utimurcin@outlook.com>
2021-03-21 11:16:55 +0300

    new operations added.

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Utilities.Results;
 using Entities;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,10 @@ namespace Business.Abstract
     public interface ICarService
     {
         IDataResult<List<Car>> GetAll();
+        IDataResult<List<CarDetailDto>> GetCarDetails();
         IDataResult<Car> GetById(int id);
+        IDataResult<CarDetailDto> GetCarDetailById(int id);
+        IDataResult<List<CarDetailDto>> GetCarsByBrand(int brandId);
         IResult Add(Car entity);
         IResult Update(Car entity);
         IResult Delete(Car entity);
--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -2,6 +2,7 @@
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,15 +37,30 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Car>>(_carDal.GetAll());
         }
 
+        public IDataResult<List<CarDetailDto>> GetCarsByBrand(int brandId)
+        {
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails(x=>x.BrandId == brandId));
+        }
+
         public IDataResult<Car> GetById(int id)
         {
             return new SuccessDataResult<Car>(_carDal.Get(x => x.Id == id));
+        }
+
+        public IDataResult<List<CarDetailDto>> GetCarDetails()
+        {
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails());
         }
 
         public IResult Update(Car entity)
         {
             _carDal.Update(entity);
             return new SuccessResult();
+        }
+
+        public IDataResult<CarDetailDto> GetCarDetailById(int id)
+        {
+            return new SuccessDataResult<CarDetailDto>(_carDal.GetCarDetailById(id));
         }
     }
 }
