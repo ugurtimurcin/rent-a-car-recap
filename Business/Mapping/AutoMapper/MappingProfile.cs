ec4f77d20690b2bc6ac41db7e4f0ec31bed62ddb
Ugur Timurcin <utimurcin@outlook.com>
2021-03-25 11:45:42 +0300

    Mapping profile created.

---
+++ Business/Mapping/AutoMapper/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Entities;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Mapping.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        { 
+             CreateMap<BrandDto, Brand>().ReverseMap();
+             CreateMap<CarDto, Car>().ReverseMap();
+             CreateMap<CarImageDto, CarImage>().ReverseMap();
+             CreateMap<ColorDto, Color>().ReverseMap();
+             CreateMap<CustomerDto, Customer>().ReverseMap();
+             CreateMap<RentalDto, Rental>().ReverseMap();
+        }
+    }
+}
